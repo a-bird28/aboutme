d3a9383ec4ba963ab7d8d246f720787f7519fd3c
Anna Byrd <aibyrd19@gmail.com>
2026-02-20 08:14:41 -0500

    projects as cards

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,10 +1,37 @@
-import project_schema from "projects.json" with { type: "json"}
-console.log(project_schema.chea_kg)
+import { Card, Grid, Typography, Accordion } from '@mui/material'
 
-export default function Home() {
+const projectCard = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'flex-start',
+  justifyContent: 'flex-start',
+  borderRadius: '1px',
+  backgroundColor: 'transparent',
+  border: '1px solid #FFFFFF',    // white border
+  borderRadius: 2,                // optional rounding
+  p: 2 
+}
+
+const projectCardTitle = {
+  fontFamily: "dejavu-sans, sans-serif",
+  fontWeight: 600,
+  fontSize: 16,
+  fontStyle: "extrabold",
+  color: "#00BA60"
+}
+
+export default function Projects() {
   return (
-    <div class='caption'>
+    <div className='caption'>
+      
       <h2> Projects </h2>
+      <Grid>
+        <Card sx={projectCard}>
+          <Typography sx={projectCardTitle}>
+            ChEA-KG: Human Transcription Factor Regulatory Network with a Knowledge Graph User Interface
+          </Typography>
+        </Card>
+      </Grid>
     </div>
   );
 }
