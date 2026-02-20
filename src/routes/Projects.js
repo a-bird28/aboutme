71be873f72d259f8b23c03ecc679df54a949aec0
Anna Byrd <aibyrd19@gmail.com>
2026-02-20 09:01:13 -0500

    project page skeleton

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,8 +1,9 @@
-import { Card, Grid, Typography, Accordion } from '@mui/material'
+import { Card, Grid, Typography, Accordion, AccordionDetails, AccordionSummary } from '@mui/material'
+import CodeIcon from '@mui/icons-material/Code'
 
 const projectCard = {
   display: 'flex',
-  flexDirection: 'row',
+  flexDirection: 'column',
   alignItems: 'flex-start',
   justifyContent: 'flex-start',
   borderRadius: '1px',
@@ -23,15 +24,72 @@ const projectCardTitle = {
 export default function Projects() {
   return (
     <div className='caption'>
-      
       <h2> Projects </h2>
-      <Grid>
-        <Card sx={projectCard}>
+      <div>
+        <Accordion sx={projectCard}>
+          <AccordionSummary
+            expandIcon={<CodeIcon color="#00BA60"/>}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
           <Typography sx={projectCardTitle}>
-            ChEA-KG: Human Transcription Factor Regulatory Network with a Knowledge Graph User Interface
+            Developing an interactive user interface for a new human gene regulatory network
           </Typography>
-        </Card>
-      </Grid>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p>
+            Associated publications:
+            </p>
+            <p>
+              <a href="https://doi.org/10.1093/bioadv/vbaf237">
+                Byrd AI*, Evangelista JE, Lachmann A, et al. ChEA-KG: Human Transcription Factor Regulatory Network with a Knowledge Graph Interactive User Interface. bioRxiv. 2025.08.09.669505 [Preprint].
+              </a>
+            </p>
+            <p>
+              <a href="https://doi.org/10.1002/cpz1.70200">
+                Evangelista JE, Lustky A, Byrd AI*, et al. Creating an Interactive Web Interface for Networks Stored in Knowledge Graph Databases. Current Protocols. 2025. 5;e70200.
+              </a>
+            </p>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion sx={projectCard}>
+          <AccordionSummary
+            expandIcon={<CodeIcon color="#00BA60"/>}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+          <Typography sx={projectCardTitle}>
+            Optimizing and debugging a single-cell analysis workflow
+          </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p>
+            Associated publication:
+            </p>
+            <p>
+              <a href="https://doi.org/10.1093/bioadv/vbaf237">
+                Marino GB, Byrd AI*, Ahmed N, et al. sc2DAT: Workflow for targeting tumor subpopulations of cells. Bioinformatics Advances. 2025. 5(1):vbaf237.
+              </a>
+            </p>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion sx={projectCard}>
+          <AccordionSummary
+            expandIcon={<CodeIcon color="#00BA60"/>}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+          <Typography sx={projectCardTitle}>
+            Developing a testing protocol for a UV sterilization device for catheter fluid
+          </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <p>
+            Bachelor of Engineering culminating project.
+            </p>
+          </AccordionDetails>
+        </Accordion>
+      </div>
     </div>
   );
 }
